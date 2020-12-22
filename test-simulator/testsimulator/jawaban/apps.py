4e4a7be827131d0b6ea5d616267c8510a9ec4732
Dandy Febriano <febrianod@gmail.com>
2020-12-22 10:25:02 +0700

    add app jawaban

---
+++ test-simulator/testsimulator/jawaban/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JawabanConfig(AppConfig):
+    name = 'jawaban'
---
+++ test-simulator/testsimulator/jawaban/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+from registration.models import UserProfile
+from pilihanJawaban.models import PilihanJawabanModel
+from soal.models import SoalModel
+# Create your models here.
+
+class JawabanModel(models.Model):
+    id_user = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
+    id_soal = models.ForeignKey(SoalModel, on_delete = models.CASCADE)
+    id_pilihan = models.ForeignKey(PilihanJawabanModel, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.jawaban
