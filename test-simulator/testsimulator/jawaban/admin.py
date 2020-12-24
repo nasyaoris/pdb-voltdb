2948322bf38abcfac6cebffd911e963f14006a5a
Dandy Febriano <febrianod@gmail.com>
2020-12-24 14:01:26 +0700

    WIP :submitting jawaban

--- test-simulator/testsimulator/jawaban/admin.py
+++ test-simulator/testsimulator/jawaban/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import JawabanModel
 
 # Register your models here.
+
+admin.site.register(JawabanModel)
--- test-simulator/testsimulator/soal/admin.py
+++ test-simulator/testsimulator/soal/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import SoalModel
 
 # Register your models here.
+
+admin.site.register(SoalModel)
