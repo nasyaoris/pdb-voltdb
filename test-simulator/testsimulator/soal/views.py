8cdb81b2150060c9bc8f479e269185c5804ef5fe
yudhapradiptar <yudhapradiptar@LAPTOP-TEDC7VV5.localdomain>
2020-12-29 21:32:39 +0700

    DONE

--- test-simulator/testsimulator/soal/views.py
+++ test-simulator/testsimulator/soal/views.py
@@ -11,94 +11,83 @@ import random
 from voltdbclient import *
 
 
-def soalPage(request,urutan):
+def soalPage(request, urutan):
     client = FastSerializer("localhost", 49154)
-    #soal = get_object_or_404(SoalModel, id=soal_id)
-    username = request.user.username
+
+    username = request.session['username']
+
+    # username = "user1" ## GANTI USERNAME NYA JADI DARI SESSION
+
     procGetSoal = VoltProcedure(client, "SelectSoalByUrutan", [FastSerializer.VOLTTYPE_STRING, FastSerializer.VOLTTYPE_INTEGER])
-    soal = procGetSoal.call(username, urutan)
-    #Get soal dari volttable
-    '''pilihan_jawaban = []
-    semua_jawaban = PilihanJawabanModel.objects.all()
-    semua_jawaban = list(semua_jawaban)'''
+    soal = procGetSoal.call([username, int(urutan)])
+
+    tableSoal = soal.tables[0]
+
+    actualSoal = tableSoal.tuples[0][0]
+
     procGetPilihan = VoltProcedure(client, "SelectPilihanBySoal", [FastSerializer.VOLTTYPE_STRING])
-    pilihan_jawaban = procGetPilihan.call(soal.id)
-    if len(pilihan_jawaban) > 0:
-        random.shuffle(pilihan_jawaban)
+    pilihan_jawaban = procGetPilihan.call([actualSoal])
+    tablePilihan = pilihan_jawaban.tables[0]
+
+    idPilihan = list(zip(*tablePilihan.tuples))[0]
+    namaPilihan = list(zip(*tablePilihan.tuples))[2]
 
     context = {
-        'pilihan_jawaban' : pilihan_jawaban,
-        'soal' : soal,
-        'soal_id' : soal.id
-        }
+        'pilihan_jawaban_id_1' : idPilihan[0],
+        'pilihan_jawaban_nama_1': namaPilihan[0],
+        'pilihan_jawaban_id_2' : idPilihan[1],
+        'pilihan_jawaban_nama_2': namaPilihan[1],
+        'pilihan_jawaban_id_3' : idPilihan[2],
+        'pilihan_jawaban_nama_3': namaPilihan[2],
+        'pilihan_jawaban_id_4' : idPilihan[3],
+        'pilihan_jawaban_nama_4': namaPilihan[3],
+        'soal' : tableSoal.tuples[0][1],
+        'soal_id' : tableSoal.tuples[0][0],
+        'urutan' : urutan
+    }
     return render(request, "soal.html", context)
 
 def submit_jawaban(request):
 
+    req = request.META.get('HTTP_REFERER')
+    urutan = int(req[-3:-1])
+    print(urutan)
+
     client = FastSerializer("localhost", 49154) ## Sesuaikan port dengan container di docker
 
     jawaban = request.POST.get('jawaban')
-    # print(jawaban)
+
     soal = request.POST.get('soal')
-    # print(soal)
-    # user = UserProfile.objects.get(nama=request.user.username)
-    username = request.user.username
-    # print(username)
+
+    username = request.session['username']
+
+    # username = "user1" ## GANTI USERNAME NYA JADI DARI SESSION
 
     procSubmitJawaban = VoltProcedure( client, "SubmitJawaban", [FastSerializer.VOLTTYPE_STRING, FastSerializer.VOLTTYPE_STRING, FastSerializer.VOLTTYPE_STRING] )
 
-    procSubmitJawaban.call(username, soal, jawaban)
-
-    procGetNextSoal = VoltProcedure( client, "SelectSoalByUrutan", [FastSerializer.VOLTTYPE_STRING, FastSerializer.VOLTTYPE_INTEGER] )
-
-    nextSoal = procGetNextSoal.call(username, ) #Need nomor urut selanjutnya
-
-    if(len(nextSoal.tables) > 0):
-        return HttpResponsedRedirect('/soal/%d/' % nextSoal)
+    procSubmitJawaban.call([username, soal, jawaban])
+    if(urutan < 50):
+        urutan +=1
+        return HttpResponseRedirect('/soal/%d/' % urutan)
     else:
-        return HttpResponsedRedirect('/soal/hasil')
-
-    # answer = JawabanModel(id_soal=SoalModel(soal), id_pilihan=PilihanJawabanModel(jawaban), id_user=user)
-    # answer.save()
-    # allSoal = SoalModel.objects.all()
-    # totalSoal = len(allSoal)
-    # allJawaban = JawabanModel.objects.filter(id_user=user)
-    # totalJawaban = len(allJawaban)
-    # answeredIdSoal = []
-    # for i in allJawaban:
-    #     answeredIdSoal.append(i.id_soal.id)
-    # if totalJawaban < totalSoal:
-    #     nextSoal = random.choice([i for i in range(1, totalSoal+1) if i not in answeredIdSoal])
-    #     return HttpResponseRedirect('/soal/%d/' % nextSoal)
-    # else:
-    #     return HttpResponseRedirect('/soal/hasil')
+        return HttpResponseRedirect('/soal/hasil')
 
 def hasil(request):
 
     client = FastSerializer("localhost", 49154)
 
-    username = request.user.username
+    username = request.session['username']
 
     procFinishTest = VoltProcedure( client, "FinishTest", [FastSerializer.VOLTTYPE_STRING])
 
-    finish = procFinishTest.call(username)
+    finish = procFinishTest.call([username])
 
     procGetScore = VoltProcedure( client, "GetFinalScore", [FastSerializer.VOLTTYPE_STRING])
 
-    score = procGetScore.call(username)
-
-    # user = UserProfile.objects.get(nama=request.user.username)
-    # jawaban = JawabanModel.objects.filter(id_user=user)
-    # totalSoal = len(SoalModel.objects.all())
-    # score = 0
-    # for ans in jawaban:
-    #     if ans.id_pilihan.id == ans.id_soal.kunci:
-    #         score += 1
-
-    # score = (score/totalSoal) * 100 
+    score = procGetScore.call([username])
 
     context = {
-        'score' : score
+        'score' : score.tables[0].tuples[0][0]
     }
 
     return render(request, "hasil.html", context)
