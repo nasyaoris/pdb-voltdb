6b620584816083f8cd5ebfe14b2c6c6e8f33612c
yudhapradiptar <50763128+yudhapradiptar@users.noreply.github.com>
2020-12-29 21:34:34 +0700

    Soal voltdb (#11)
    * add voltdbclient.py
    
    * score done, submit almost
    
    * score done, submit almost
    
    * change finish status
    
    * add log ignore
    
    * DONE
    
    Co-authored-by: yudhapradiptar <yudhapradiptar@LAPTOP-TEDC7VV5.localdomain>

--- test-simulator/testsimulator/registration/views.py
+++ test-simulator/testsimulator/registration/views.py
@@ -5,27 +5,34 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
+from voltdbclient import *
 
 
 def loginPage(request):
-    # if (request.session['username'] != ''):
-    #     return HttpResponseRedirect('/registration/success')
     return render(request, "login.html")
 
 
 def login(request):
+
+    client = FastSerializer("localhost", 49154)
+
+    if request.method == "POST":
+        client = FastSerializer("localhost", 49154)
+
     username = request.POST.get('username')
     password = request.POST.get('password')
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        auth_login(request, user)
-        request.session['username'] = username
-        # Redirect to a success page.
-        return HttpResponseRedirect('/registration/success')
-    else:
-        # Return an 'invalid login' error message.
-        return HttpResponseRedirect('/registration/login')
 
+    proc = VoltProcedure(client, "Login", [FastSerializer.VOLTTYPE_STRING, FastSerializer.VOLTTYPE_STRING])
+    user = proc.call([username, password])
+
+    table = user.tables[0]
+
+    try:
+        if (table.tuples[0][0] == username):
+            request.session['username'] = table.tuples[0][0]
+            return HttpResponseRedirect('/soal/1')
+    except:
+        return HttpResponseRedirect('/registration/loginPage')
 
 def successPage(request):
     return render(request, "success.html")
